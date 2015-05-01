c6c7cd1d7a75b2a61b1576a0af5557ff07616086
Georg Obermayr <g.obermayr@adverma.de>
2015-05-01 20:26:24 +0200

    Comments & read me for index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
     critical: {
       test: {
         options: {
-          base: './',
+          base: '',
           css: [
             'css/style.min.css'
           ]
